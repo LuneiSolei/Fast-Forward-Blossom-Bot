2391342f79bbf01fdfd4774c49b561faaf8811c5
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 17:07:19 -0500

    Update github_pull_request.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/github_pull_request.sh
+++ scripts/github_pull_request.sh
@@ -8,13 +8,11 @@ GITHUB_PR=$(mktemp)
 # Returns the first non-null value from the provided paths
 function github_pull_request {
     # Attempt to get PR URL from issue_comment first, then pull_request event data
-    PR_URL="$(github_event .issue.pull_request.url .pull_request.url)"
-    if [ -z "${PR_URL}" ] || [ "${PR_URL}" = "null" ]
-    then
+    PR_URL="$(github_event .issue.pull_request.url .pull_request.url)" || {
         # Nothing was found
         echo "Unable to find pull request's context." >&2
         exit 1
-    fi
+    }
 
     # Using our newly found URL, get the full PR object
     curl --silent --show-error --location --globoff \
