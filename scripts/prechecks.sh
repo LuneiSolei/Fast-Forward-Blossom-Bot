d9d2df9167eb0d030d8b4e845da3a92298eee5f4
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 17:41:02 -0500

    Update prechecks.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/prechecks.sh
+++ scripts/prechecks.sh
@@ -4,17 +4,18 @@ set -e
 
 # Ensure COMMENT value is valid
 case "${COMMENT}" in
-    always|on-error|never) ;;
-    *)
-        echo "::error::Invalid value '${COMMENT}' for COMMENT." >&2
-        exit 1
-        ;;
+  always|on-error|never) ;;
+  *)
+    echo "::error::Invalid value '${COMMENT}' for COMMENT." >&2
+    exit 1
+    ;;
 esac
 
 # Ensure DEBUG value is valid
 case "${DEBUG}" in
-    true|false) ;;
-    *) echo "::error::Invalid value '${DEBUG}' for DEBUG." >&2
+  true|false) ;;
+  *) 
+    echo "::error::Invalid value '${DEBUG}' for DEBUG." >&2
     exit 1
     ;;
 esac
@@ -22,36 +23,36 @@ esac
 # Ensure GITHUB_TOKEN is set
 if [[ -z "${GITHUB_TOKEN}" ]]
 then
-    echo "::error::Invalid value '${GITHUB_TOKEN}' for GITHUB_TOKEN." >&2
-    exit 1
+  echo "::error::Invalid value '${GITHUB_TOKEN}' for GITHUB_TOKEN." >&2
+  exit 1
 fi
 
 # Ensure AUTO_MERGE is valid
 case "${AUTO_MERGE}" in
-    true|false) ;;
-    *)
-        echo "::error::Invalid value '${AUTO_MERGE}' for AUTO_MERGE." >&2
-        exit 1
-        ;;
+  true|false) ;;
+  *)
+    echo "::error::Invalid value '${AUTO_MERGE}' for AUTO_MERGE." >&2
+    exit 1
+    ;;
 esac
 
 
 # Ensure we're running via GitHub Actions
 if [[ -z "${GITHUB_EVENT_PATH}" ]]
 then
-    echo "::error::GITHUB_EVENT_PATH environment variable must be set."
-    exit 1
+  echo "::error::GITHUB_EVENT_PATH environment variable must be set."
+  exit 1
 fi
 
 # Do some logging if DEBUG is enabled
 if [[ "${DEBUG}" == "true" ]]
 then
-    {
-        echo "env"
-        env
-        echo "GITHUB_ENV: ${GITHUB_ENV}"
-        cat "${GITHUB_ENV}"
-        echo "GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}"
-        cat "${GITHUB_EVENT_PATH}"
-    } >&2
+  {
+    echo "env"
+    env
+    echo "GITHUB_ENV: ${GITHUB_ENV}"
+    cat "${GITHUB_ENV}"
+    echo "GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}"
+    cat "${GITHUB_EVENT_PATH}"
+  } >&2
 fi
