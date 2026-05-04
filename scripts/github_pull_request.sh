388d65a95241461d30c7c06556c3e89857b615e8
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 17:11:51 -0500

    Update github_pull_request.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/github_pull_request.sh
+++ scripts/github_pull_request.sh
@@ -6,43 +6,44 @@ set -e
 GITHUB_PR=$(mktemp)
 
 # Returns the first non-null value from the provided paths
-function github_pull_request {
-    # Attempt to get PR URL from issue_comment first, then pull_request event data
-    PR_URL="$(github_event .issue.pull_request.url .pull_request.url)" || {
-        # Nothing was found
-        echo "Unable to find pull request's context." >&2
-        exit 1
-    }
+# Attempt to get PR URL from issue_comment first, then pull_request event data
+PR_URL="$(./github_event.sh .issue.pull_request.url .pull_request.url)" || {
+    # Nothing was found
+    echo "::error::Unable to find pull request's context." >&2
+    exit 1
+}
 
-    # Using our newly found URL, get the full PR object
-    curl --silent --show-error --location --globoff \
-    -X GET \
-    -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-    -H "X-GitHub-Api-Version: 2026-03-10" \
-    "${PR_URL}" >${GITHUB_PR}
+# Using our newly found URL, get the full PR object
+curl --silent --show-error --location --globoff \
+-X GET \
+-H "Accept: application/vnd.github+json" \
+-H "Authorization: Bearer ${GITHUB_TOKEN}" \
+-H "X-GitHub-Api-Version: 2026-03-10" \
+"${PR_URL}" >${GITHUB_PR}
 
-    # Do some debug logging, if enabled
-    if [ "${DEBUG}" -gt 0 ]
+# Do some debug logging, if enabled
+if [[ "${DEBUG}" == "false" ]]
+then
+    {
+        echo "pull_request (${GITHUB_PR}):"
+        cat "${GITHUB_PR}"
+    } >&2
+fi
+
+# Extract field(s) from our now cached PR data by returning
+# the first non-null value from the provided paths
+while [[ "$#" > 0 ]]
+do
+    VALUE=$(jq -r "$1" <${GITHUB_PR})
+    if [ -n "${VALUE}" ] && [ "${VALUE}" != null ]
     then
-        {
-            echo "pull_request (${GITHUB_PR}):"
-            cat "${GITHUB_PR}"
-        } >&2
+        echo "${VALUE}"
+        exit 0
     fi
 
-    # Extract field(s) from our now cached PR data by returning
-    # the first non-null value from the provided paths
-    while [ "$#" -gt 0 ]
-    do
-        VALUE=$(jq -r "$1" <${GITHUB_PR})
-        if [ -n "${VALUE}" ] && [ "${VALUE}" != null ]
-        then
-            echo "${VALUE}"
-            break
-        fi
+    # This was empty, try the next path
+    shift
+done
 
-        # This was empty, try the next path
-        shift
-    done
-}
+# Nothing was found
+exit 1
