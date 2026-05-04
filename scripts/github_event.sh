4bea532c6602b1c6b78a3bd1cf8304c0471589e7
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-04 17:07:37 -0500

    Update github_event.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/github_event.sh
+++ scripts/github_event.sh
@@ -3,18 +3,16 @@
 set -e
 
 # Returns the value of the first key in the github.event data structure that is not null.
-function github_event {
-    while [ "$#" -gt 0 ]
-    do
-        VALUE=$(jq -r "${1}" <"${GITHUB_EVENT_PATH}")
-        if [ -n "${VALUE}" ] && [ "${VALUE}" != "null" ]
-        then
-            echo "${VALUE}"
-            return 0
-        fi
+while [ "$#" -gt 0 ]
+do
+    VALUE=$(jq -r "${1}" <"${GITHUB_EVENT_PATH}")
+    if [ -n "${VALUE}" ] && [ "${VALUE}" != "null" ]
+    then
+        echo "${VALUE}"
+        return 0
+    fi
 
-        shift
-    done
+    shift
+done
 
-    return 1
-}
+return 1
