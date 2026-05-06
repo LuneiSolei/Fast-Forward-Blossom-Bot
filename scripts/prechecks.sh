ce61b2ed514bce467882736bbbaf7de01b2eb00a
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 20:20:33 -0500

    Update prechecks.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/prechecks.sh
+++ scripts/prechecks.sh
@@ -5,10 +5,10 @@ set -e
 # Ensure COMMENT value is valid
 case "${COMMENT}" in
   always|on-error|never) 
-    echo "COMMENT=${COMMENT}" >> "${GITHUB_ENV}"
+    printf 'COMMENT=%s\n' "${COMMENT}" >> "${GITHUB_ENV}"
     ;;
   *)
-    echo "::error::Invalid value '${COMMENT}' for COMMENT." >&2
+    printf '::error::Invalid value \'%s\' for COMMENT\n' "${COMMENT}" >&2
     exit 1
     ;;
 esac
@@ -16,19 +16,19 @@ esac
 # Ensure GITHUB_TOKEN is set
 if [[ -z "${GITHUB_TOKEN}" ]]
 then
-  echo "::error::Invalid value '${GITHUB_TOKEN}' for GITHUB_TOKEN." >&2
+  printf '::error::Invalid value \'%s\' for GITHUB_TOKEN\n' "${GITHUB_TOKEN}" >&2
   exit 1
 else
-  echo "GITHUB_TOKEN=${GITHUB_TOKEN}" >> "${GITHUB_ENV}"
+  printf 'GITHUB_TOKEN=%s\n' "${GITHUB_TOKEN}" >> "${GITHUB_ENV}"
 fi
 
 # Ensure AUTO_MERGE is valid
 case "${AUTO_MERGE}" in
   true|false) 
-    echo "AUTO_MERGE=${AUTO_MERGE}" >> "${AUTO_MERGE}"
+    printf 'AUTO_MERGE=%s\n' "${AUTO_MERGE}" >> "${GITHUB_ENV}"
     ;;
   *)
-    echo "::error::Invalid value '${AUTO_MERGE}' for AUTO_MERGE." >&2
+    printf '::error::Invalid value for \'%s\' for AUTO_MERGE\n' "${AUTO_MERGE}" >&2
     exit 1
     ;;
 esac
@@ -36,7 +36,7 @@ esac
 # Ensure we're running via GitHub Actions
 if [[ -z "${GITHUB_EVENT_PATH}" ]]
 then
-  echo "::error::GITHUB_EVENT_PATH environment variable must be set."
+  printf '::error::GITHUB_EVENTPATH environment variable must be set' >&2
   exit 1
 fi
 
