57df0015a6cf5b40dd6074a5880810f054b3f592
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 20:30:49 -0500

    Update fast-forward.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/fast-forward.sh
+++ scripts/fast-forward.sh
@@ -2,37 +2,3 @@
 
 set -e
 
-EXIT_CODE=$(mktemp)
-echo 1 >${EXIT_CODE}
-
-# Export the comment as a GitHub Actions output variable.
-{
-  echo "comment<<EOF_${COMMENT_CONTENT}"
-  cat "${COMMENT_CONTENT}"
-  echo "EOF_${COMMENT_CONTENT}"
-} | tee -a "${GITHUB_OUTPUT}"
-
-# Determine whether to post the comment based on the setting
-if [ "${COMMENT}" = "always" ] \
-    || [ "${COMMENT}" = "on-error" -a "$(cat ${EXIT_CODE})" -ne 0 ]
-then
-  # Post the comment.
-  COMMENTS_URL="$(${GITHUB_ACTION_PATH}/scripts/github-pull-request.sh .comments_url)"
-  if [ -n "${COMMENTS_URL}" ]
-  then
-    echo "Posting comment to ${COMMENTS_URL}."
-    curl --silent --show-error --location --globoff \
-      -X POST \
-      -H "Accept: application/vnd.github+json" \
-      -H "Authorization: Bearer ${GITHUB_TOKEN}" \
-      -H "X-GitHub-Api-Version: 2026-03-10" \
-      "${COMMENTS_URL}" \
-      -d "@${COMMENT_CONTENT}"
-  else
-      echo "Can't post a comment: github.event.pull_request.comments_url is not set." | tee -a ${GITHUB_STEP_SUMMARY}
-  fi
-else
-    echo "Not posting comment."
-fi
-
-exit $(cat ${EXIT_CODE})
