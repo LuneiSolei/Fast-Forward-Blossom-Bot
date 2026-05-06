6fa9cf5e88efba1ed518f6233a04145d8a9e7ab7
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 19:55:03 -0500

    Update perform-merge.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/perform-merge.sh
+++ scripts/perform-merge.sh
@@ -1,24 +1,24 @@
-#! /bin/bash
+#!/bin/bash
 
 set -e
 
-
-
+PUSH_LOG=$(mktemp)
 if [[ "${HAS_PERMS}" == "true" ]] && [[ "${IS_POSSIBLE}" == "true" ]] && [[ "${AUTO_MERGE}" == "true" ]]
 then
-  echo -n "Fast Forwarding \`${BASE_REF}\` (${BASE_SHA}) to"
-  echo " \`${HEAD_REF}\` (${HEAD_SHA})."
-
-  echo '```shell'
-  (
-    # Show the git command in the output
-    PS4='$ '
-    set -x
-
-    # Push the commit directly to the base branch.
-    # This fast-forwards BASE_REF to point to HEAD_SHA
-    git push origin "${HEAD_SHA}:${BASE_REF}"
-  )
-  echo '```'
-  echo "EXIT_CODE=0" >> ${GITHUB_ENV}
+  {
+    echo -n "Fast Forwarding \`${BASE_REF}\` (${BASE_SHA}) to"
+    echo " \`${HEAD_REF}\` (${HEAD_SHA})."
+  
+    echo '```shell'
+    (
+      # Show the git command in the output
+      PS4='$ '
+      set -x
+  
+      # Push the commit directly to the base branch.
+      # This fast-forwards BASE_REF to point to HEAD_SHA
+      git push origin "${HEAD_SHA}:${BASE_REF}"
+    )
+    echo '```'
+  } 2>&1 | tee "${PUSH_LOG}"
 fi
