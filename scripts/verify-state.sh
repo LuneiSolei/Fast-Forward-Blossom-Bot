ef82cf114983d94741c76380e209c96c9676c01c
Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-05 19:39:30 -0500

    Update verify-state.sh
    Signed-off-by: Lunei Solei <68037318+LuneiSolei@users.noreply.github.com>

--- scripts/verify-state.sh
+++ scripts/verify-state.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-set -e 
+set -e
+
+# Check that user has permission to push to the repository.
+COLLABORATORS_URL="$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .repository.collaborators_url)"
+
+# Build URL used to check for permissions
+COLLABORATORS_URL="${COLLABORATORS_URL}%\{/collaborator\}"
+
+# Fetch the user's permissions from GitHub API
+PERM=$(mktemp)
+curl --silent --show-error -o "${PERM}" --location --globoff \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GITHUB_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2026-03-10" \
+  "${COLLABORATORS_URL}/$(${GITHUB_ACTION_PATH}/scripts/github-event.sh .sender.login)/permission"
+
+# Output push permissions state
+printf 'has-perms=%s\n' "$(jq -r .user.permissions.push < ${PERM})" >> "${GITHUB_OUTPUT}"
 
 # Verify if fast forwarding is possible
 # Create a local branch reference for the PR
 git branch -f "pull_request/${HEAD_REF}" "${HEAD_SHA}"
 
 # Check if the base branch is a direct ancestor of the head branch
-if ! git merge-base --is-ancestor "${BASE_REF}" "${HEAD_SHA}"
-then
-  printf 'is-possible=%s\n' "false" >> "${GITHUB_OUTPUT}"
-elif [[ "${AUTO_MERGE}" == "true" ]]
-then
-  printf 'is-possible=%s\n' "true" >> "${GITHUB_OUTPUT}"
-fi
+IS_POSSIBLE=! git merge-base --is-ancestor "${BASE_REF}" "${HEAD_SHA}"
+printf 'is-possible=%s\n' "${IS_POSSIBLE}" >> "${GITHUB_OUTPUT}"
