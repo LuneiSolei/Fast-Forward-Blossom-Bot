3507354b84ae43edcaa662b04cdec821570dd348
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-08 12:48:03 -0500

    fix: improve error handling/output in prechecks.sh

--- scripts/prechecks.sh
+++ scripts/prechecks.sh
@@ -8,15 +8,16 @@ case "${COMMENT}" in
     printf "COMMENT=%s\n" "${COMMENT}" >> "${GITHUB_ENV}"
     ;;
   *)
-    printf "::error::Invalid value '%s' for COMMENT\n" "${COMMENT}" >&2
+    printf "::error::Invalid value '%s' for COMMENT\n" "${COMMENT}" >> "${GITHUB_STEP_SUMMARY}" >&2
     exit 1
 esac
 
+SHOULD_EXIT=false
 # Ensure GITHUB_TOKEN is set
 if [[ -z "${GITHUB_TOKEN}" ]]
 then
   printf "::error::GITHUB_TOKEN cannot be empty" >&2
-  exit 1
+  SHOULD_EXIT=true
 else
   printf "GITHUB_TOKEN=%s\n" "${GITHUB_TOKEN}" >> "${GITHUB_ENV}"
 fi
@@ -27,20 +28,27 @@ case "${AUTO_MERGE}" in
     printf "AUTO_MERGE=%s\n" "${AUTO_MERGE}" >> "${GITHUB_ENV}"
     ;;
   *)
-    printf "::error::Invalid value '%s' for AUTO_MERGE\n" "${AUTO_MERGE}" >&2
-    exit 1
+    printf "::error::Invalid value '%s' for AUTO_MERGE\n" "${AUTO_MERGE}" >> "${GITHUB_STEP_SUMMARY}" >&2
+    SHOULD_EXIT=true
     ;;
 esac
 
 # Ensure we're running via GitHub Actions
 if [[ -z "${GITHUB_EVENT_PATH}" ]]
 then
-  printf "::error::GITHUB_EVENTPATH environment variable must be set" >&2
+  printf "::error::GITHUB_EVENT_PATH environment variable not set. This script is intended to be run within a GitHub
+    Actions workflow." >> "${GITHUB_STEP_SUMMARY}" >&2
+  SHOULD_EXIT=true
+fi
+
+# Debug output
+printf "::debug::GITHUB_ENV: %s\n" "${GITHUB_ENV}"
+printf "::debug::GITHUB_EVENT_PATH: %s\n" "${GITHUB_EVENT_PATH}"
+
+if [[ "${SHOULD_EXIT}" == "true" ]]
+then
   exit 1
 fi
 
-echo "::debug::env"
-echo "::debug::GITHUB_ENV: ${GITHUB_ENV}"
-echo "::debug::${GITHUB_ENV}"
-echo "::debug::GITHUB_EVENT_PATH: ${GITHUB_EVENT_PATH}"
-echo "::debug::${GITHUB_EVENT_PATH}"
+# Step summary output
+printf "Pre-checks completed successfully.\n" >> "${GITHUB_STEP_SUMMARY}"
