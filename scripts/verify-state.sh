66ad7da5e5a25261f0e90f3a95250bf431a75889
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-08 14:08:37 -0500

    fix: correct collaborators url parsing

--- scripts/verify-state.sh
+++ scripts/verify-state.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-# Check that user has permission to push to the repository.
-COLLABORATORS_URL="$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .repository.collaborators_url)"
-
 # Build URL used to check for permissions
-COLLABORATORS_URL="${COLLABORATORS_URL/\{\/collaborator\}/}"
+COLLABORATORS_URL="$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .repository.collaborators_url)"
+USERNAME="$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .sender.login)"
+COLLABORATORS_URL="${COLLABORATORS_URL/\{\/collaborator\}/}/${USERNAME}"
 
 # Fetch the user's permissions from GitHub API
 PERM=$(mktemp)
@@ -14,9 +13,9 @@ curl --silent --show-error -o "${PERM}" --location --globoff \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${GITHUB_TOKEN}" \
   -H "X-GitHub-Api-Version: 2026-03-10" \
-  "${COLLABORATORS_URL}/$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .sender.login)/permission"
+  "${COLLABORATORS_URL}"
 
-echo "${COLLABORATORS_URL}/$("${GITHUB_ACTION_PATH}"/scripts/github-event.sh .sender.login)/permission"
+echo "${COLLABORATORS_URL}"
 
 # Output push permissions state
 printf "HAS_PERMS=%s\n" "$(jq -r .user.permissions.push < "${PERM}")" >> "${GITHUB_ENV}"
