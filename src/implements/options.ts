45d78f6538a4347b00f6b4103594eddc9873d0c5
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-05-27 21:00:57 +0000

    chore: move property initialization into property getters

--- src/implements/options.ts
+++ src/implements/options.ts
@@ -1,5 +1,6 @@
 import * as core from "@actions/core";
 import type IOptions from "../core/actionInfo/IOptions.js";
+import {Logger} from "../core/Logger.js";
 
 export default class Options implements IOptions {
     private static _options: core.InputOptions = {
@@ -7,34 +8,36 @@ export default class Options implements IOptions {
         trimWhitespace: true
     };
 
-    private readonly _autoMerge: boolean;
-    private readonly _postComment: string;
-    private readonly _customCommand: string;
-
-    public constructor() {
-        this._autoMerge = core.getBooleanInput("auto_merge", Options._options);
-        this._customCommand = core.getInput("custom_command", Options._options);
-        this._postComment = core.getInput("post_comment", Options._options);
-
-        if (this._postComment !== "always" && this._postComment !== "on-error" && this._postComment !== "never") {
-            core.setFailed(`Invalid value '${this._postComment}' for workflow input 'comment'`)
-        }
-
-        return this;
-    }
+    private _autoMerge?: boolean;
+    private _postComment?: string;
+    private _customCommand?: string;
 
     public get AutoMerge(): boolean
     {
+        if (this._autoMerge) return this._autoMerge;
+
+        this._autoMerge = core.getBooleanInput("auto_merge", Options._options);
         return this._autoMerge;
     }
 
     public get CustomCommand(): string
     {
+        if (this._customCommand) return this._customCommand;
+
+        this._customCommand = core.getInput("custom_command", Options._options);
         return this._customCommand;
     }
 
     public get PostComment(): string
     {
+        if (this._postComment) return this._postComment;
+
+        this._postComment = core.getInput("post_comment", Options._options);
+
+        if (this._postComment !== "always" && this._postComment !== "on-error" && this._postComment !== "never") {
+            Logger.InvalidWorkflowValueError("post_comment", this._postComment, 1);
+        }
+
         return this._postComment;
     }
 }
